30dcccdb99f0aca10da10bc5c5fe846ddc8289bd
Dongfeng Yu <60870777+blockader@users.noreply.github.com>
2020-07-20 17:12:37 +0800

    Refining HID

--- users/matroid/client/install.sh
+++ users/matroid/client/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -xe
 os=`uname`
 if [ $os == "Darwin" ]; then
-    pip install hidapi
-    pip install pync
-    pip install pyqt5
+    brew install hidapi
 else
     echo $os is not a supported OS.
 fi
